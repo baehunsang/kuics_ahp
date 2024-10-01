0ca825c078d083e939e6dd0c48f70a524ebee42b
hunsang <hoonsang99@korea.ac.kr>
2024-10-02 02:37:38 +0900

    Final
    Signed-off-by: hunsang <hoonsang99@korea.ac.kr>

--- src/rop_basic.c
+++ src/rop_basic.c
@@ -57,22 +57,21 @@ static ssize_t module_write(struct file *file, const char __user *buf,
     }
     memcpy(g_buf, kbuf, BUFFER_SIZE);
 	//check
-	uint64_t* ret = (uint64_t*)(kbuf + 0x408);
-    if((ret[0] >= 0xffffffff81c00df0 && ret[0] < 0xffffffff82000000)||
-        (ret[1] >= 0xffffffff81c00df0 && ret[1] < 0xffffffff82000000)||
-        (ret[2] >= 0xffffffff81c00df0 && ret[2] < 0xffffffff82000000)||
-        (ret[3] >= 0xffffffff81c00df0 && ret[3] < 0xffffffff82000000)||
-        (ret[4] >= 0xffffffff81c00df0 && ret[4] < 0xffffffff82000000)||
-        (ret[5] >= 0xffffffff81c00df0 && ret[5] < 0xffffffff82000000)||
-        (ret[6] >= 0xffffffff81c00df0 && ret[6] < 0xffffffff82000000)||
-        (ret[7] >= 0xffffffff81c00df0 && ret[7] < 0xffffffff82000000)||
-        (ret[8] >= 0xffffffff81c00df0 && ret[8] < 0xffffffff82000000)||
-        (ret[9] >= 0xffffffff81c00df0 && ret[9] < 0xffffffff82000000)||
-        (ret[10] >= 0xffffffff81c00df0 && ret[10] < 0xffffffff82000000)||
-        (ret[11] >= 0xffffffff81c00df0 && ret[11] < 0xffffffff82000000)
+	uint64_t* ret = (uint64_t*)(&kbuf[0x408]);
+    if(((ret[0] >= 0xffffffff81c00df0) && (ret[0] <  0xffffffff82450000))||
+        ((ret[1] >= 0xffffffff81c00df0) && (ret[1] < 0xffffffff82450000))||
+        ((ret[2] >= 0xffffffff81c00df0) && (ret[2] < 0xffffffff82450000))||
+        ((ret[3] >= 0xffffffff81c00df0) && (ret[3] < 0xffffffff82450000))||
+        ((ret[4] >= 0xffffffff81c00df0) && (ret[4] < 0xffffffff82450000))||
+        ((ret[5] >= 0xffffffff81c00df0) && (ret[5] < 0xffffffff82450000))||
+        ((ret[6] >= 0xffffffff81c00df0) && (ret[6] < 0xffffffff82450000))||
+        ((ret[7] >= 0xffffffff81c00df0) && (ret[7] < 0xffffffff82450000))||
+        ((ret[8] >= 0xffffffff81c00df0) && (ret[8] < 0xffffffff82450000))||
+        ((ret[9] >= 0xffffffff81c00df0) && (ret[9] < 0xffffffff82450000))||
+        ((ret[10] >= 0xffffffff81c00df0) && (ret[10] < 0xffffffff82450000))||
+        ((ret[11] >= 0xffffffff81c00df0) && (ret[11] < 0xffffffff82450000))
     ){
-			printk(KERN_INFO "No ROP!");
-			return -EINVAL;
+			panic("No rop!");
 		}
 		
     return count;
