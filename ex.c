4f4d405894212af75551c7b0cf5f223db4c263a6
hunsang <hoonsang99@korea.ac.kr>
2024-10-30 23:56:08 +0900

    WEEK$: add hexdump
    Signed-off-by: hunsang <hoonsang99@korea.ac.kr>

--- ex.c
+++ ex.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#ifndef HEXDUMP_COLS
+#define HEXDUMP_COLS 16
+#endif
 
 #define is_error(c, s)          \
     {                           \
@@ -14,6 +17,51 @@
 
 #define init_cred 0xffffffff824505e0 
 #define commit_creds  0xffffffff8108cd90
+
+void hexdump(void *mem, unsigned int len)
+{
+	unsigned int i, j;
+
+	for(i = 0; i < len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0); i++)
+	{
+		/* print offset */
+		if(i % HEXDUMP_COLS == 0)
+		{
+			printf("0x%06x: ", i);
+		}
+
+		/* print hex data */
+		if(i < len)
+		{
+			printf("%02x ", 0xFF & ((char*)mem)[i]);
+		}
+		else /* end of block, just aligning for ASCII dump */
+		{
+			printf("   ");
+		}
+
+		/* print ASCII dump */
+		if(i % HEXDUMP_COLS == (HEXDUMP_COLS - 1))
+		{
+			for(j = i - (HEXDUMP_COLS - 1); j <= i; j++)
+			{
+				if(j >= len) /* end of block, not really printing */
+				{
+					putchar(' ');
+				}
+				else if(isprint(((char*)mem)[j])) /* printable char */
+				{
+					putchar(0xFF & ((char*)mem)[j]);        
+				}
+				else /* other char */
+				{
+					putchar('.');
+				}
+			}
+			putchar('\n');
+		}
+	}
+}
 
 unsigned long user_cs, user_ss, user_rsp, user_rflags;
 
