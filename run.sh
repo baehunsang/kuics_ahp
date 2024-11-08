c177502bae7bf391041d1e467ff29afe58367794
hunsang <hoonsang99@korea.ac.kr>
2024-11-08 16:44:59 +0900

    INIT week5
    Signed-off-by: hunsang <hoonsang99@korea.ac.kr>

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ qemu-system-x86_64 \
     -m 64M \
     -nographic \
     -kernel bzImage \
-    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on" \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on nokaslr" \
     -no-reboot \
     -cpu qemu64,+smep,+smap \
     -smp 1 \
