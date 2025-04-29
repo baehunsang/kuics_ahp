a5daf4daaa7fc9fbce20683066a188750d7f0ece
hunsang <hoonsang99@korea.ac.kr>
2025-04-29 20:08:11 +0900

    EDIT gitignore

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@
 qemu-system-x86_64 \
     -m 64M \
     -nographic \
-    -kernel bzImage \
-    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on kaslr" \
+    -kernel ./bzImage_debug \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on nokaslr" \
     -no-reboot \
     -cpu qemu64,+smap,+smep \
     -monitor /dev/null \
-    -initrd rootfs.cpio \
+    -initrd ./rootfs_updated.cpio \
     -net nic,model=virtio \
     -net user \
-	-S -s
