a11162d90b3cfc4d36ac5e8e788004759e16be6f
hunsang <hoonsang99@korea.ac.kr>
2025-04-29 15:17:23 +0900

    init setup

--- run.sh
+++ run.sh
@@ -3,12 +3,11 @@ qemu-system-x86_64 \
     -m 64M \
     -nographic \
     -kernel bzImage \
-    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on" \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on kaslr" \
     -no-reboot \
-    -cpu qemu64,+smep,+smap \
-    -smp 2\
+    -cpu qemu64,+smap,+smep \
     -monitor /dev/null \
-    -initrd ./rootfs_updated.cpio \
+    -initrd rootfs.cpio \
     -net nic,model=virtio \
     -net user \
-	-gdb tcp::12345
+	-S -s
